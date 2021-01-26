9e614854c29e959133f51ed4f6807c0fcf963a21
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-01-25 21:20:39 -0500

    max inventory size helper

---
+++ src/utils/inventoryUpdate.ts
@@ -0,0 +1,22 @@
+import { User } from "discord.js";
+import users from "../models/User";
+
+/**
+ * Adds an item to a user's inventory, if it is not full.
+ * @param user the user to add the item to
+ * @param item the item to add
+ * @returns whether or not the operation was succesful
+ */
+export async function addInventoryItem(user: User, item: any) {
+    const { inventory } = await users.findById(user.id);
+    if (inventory.length >= 50) return false;
+
+    await users.findByIdAndUpdate(user.id, {
+        _id: user.id,
+        $push: {
+            inventory: item,
+        },
+    });
+
+    return true;
+}
