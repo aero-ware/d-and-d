6a6aece8de85242cb5528dc6855c33eacf7a228d
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-01-25 20:09:11 -0500

    started economy utils

---
+++ src/utils/eco.ts
@@ -0,0 +1,32 @@
+import { User } from "discord.js";
+
+import users from "../models/User";
+
+/**
+ * Returns the balance of the specificed user.
+ * @param user the user to get the balance of
+ */
+export async function getBal(user: User): Promise<number> {
+    const { balance } = await users.findById(user.id);
+    return balance || 0;
+}
+
+/**
+ * Adds the given number of coins to the user. Provide a negative number to remove coins.
+ * @param user the user to add balance to
+ * @param coins the number of coins to add
+ */
+export async function addBal(user: User, coins: number): Promise<number> {
+    if (!Number.isInteger(coins)) throw new TypeError("parameter 'coins' must be an integer.");
+    const { balance } = await users.findByIdAndUpdate(user.id, {
+        id: user.id,
+        $inc: {
+            balance: coins,
+        },
+    }, {
+        new: true,
+        setDefaultsOnInsert: true,
+    });
+
+    return balance;
+}
